6a5f30f9b59a77ab5c194226fbf11d0bbef74f54
Roman Acevedo <roman.acevedo62@gmail.com>
2025-11-24 20:04:35 +0100

    feat(executions): revert index, too specific usecase

--- jdbc-h2/src/main/resources/migrations/h2/V1_48__executions_end_date_sort_index.sql
+++ jdbc-h2/src/main/resources/migrations/h2/V1_48__executions_end_date_sort_index.sql
@@ -1,4 +1,2 @@
 -- make state_duration nullable
 ALTER TABLE executions ALTER COLUMN "state_duration" DROP NOT NULL;
--- speed up sorting by duration, it should work for every combination of ASC/DESC and NULLS FIRST/LAST
-CREATE INDEX IF NOT EXISTS executions_state_duration_sort ON executions ("state_duration" ASC NULLS FIRST);
